18c7f96388a55a8cd7ecbeba0b81abc208874768
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-01-12 15:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digits) {
   //Complete the function
+	if (!digits) {
+        return [];
+    }
+    let result = [''];
+    for (let i = 0; i < digits.length; i++) {
+        let letters = map[digits[i]];
+        let temp = [];
+        for (let j = 0; j < letters.length; j++) {
+            for (let k = 0; k < result.length; k++) {
+                temp.push(result[k] + letters[j]);
+            }
+        }
+        result = temp;
+    }
+    return result;
 }
 
 module.exports = letterCombinations;
